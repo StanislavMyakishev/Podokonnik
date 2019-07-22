45882587f6c7fd4892826772b2f0fcf9afad056b
StanislavMyakishev <masiff-s@live.ru>
2019-07-22 15:14:05 +0300

    new pages: Login + Registartion

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home.vue'
+import Login from '../components/auth/Login.vue'
+import Registration from '../components/auth/Registration.vue'
 
 Vue.use(Router)
 
@@ -10,7 +12,18 @@ export default new Router({
             path: '',
             name: 'Home',
             component: Home
-        }
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/registartion',
+            name: 'Registration',
+            component: Registration
+        },
+
     ],
     mode: 'history',
 })
