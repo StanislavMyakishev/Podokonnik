f6218e74fe9ffbc11e46790e45bbc1f1f0cab3af
ksercs <ksercs0@gmail.com>
2019-07-24 14:28:14 +0300

    api for login/logout added

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -5,6 +5,11 @@ from allauth.account.views import ConfirmEmailView
 from .views import AdView, SingleAdView, CategoryView, SingleCategoryView#, CreateUserView
 from . import views
 
+from rest_auth.views import (
+    LoginView, LogoutView, UserDetailsView, PasswordChangeView,
+    PasswordResetView, PasswordResetConfirmView
+)
+
 urlpatterns = [
     url('ads/', AdView.as_view()),
     url('ads/<int:pk>', SingleAdView.as_view()),
@@ -20,5 +25,17 @@ urlpatterns = [
     url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.null_view, name='password_reset_confirm'),
     # Default urls
     url(r'', include('rest_auth.urls')),
-    url(r'^registration/', include('rest_auth.registration.urls'))
+    url(r'^registration/', include('rest_auth.registration.urls')),
+
+    # URLs that do not require a session or valid token
+    url(r'^password/reset/$', PasswordResetView.as_view(),
+        name='rest_password_reset'),
+    url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
+        name='rest_password_reset_confirm'),
+    url(r'^login/$', LoginView.as_view(), name='rest_login'),
+    # URLs that require a user to be logged in with a valid session / token.
+    url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
+    url(r'^user/$', UserDetailsView.as_view(), name='rest_user_details'),
+    url(r'^password/change/$', PasswordChangeView.as_view(),
+        name='rest_password_change'),
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,10 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 
